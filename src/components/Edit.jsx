2899fb4edf47a2103066c7578ed6918b5ef764b9
Mahdi Yazdani <mahdiyazdani@msn.com>
2020-11-11 06:54:45 +0300

    Filter and generate a list post-type select-options

--- src/components/Edit.jsx
+++ src/components/Edit.jsx
@@ -1,9 +1,12 @@
 /**
  * External dependencies
  */
+import { concat, chain, filter, map, mapKeys, pick } from 'lodash';
 import classnames from 'classnames';
 import restFetch from './../utils/rest-fetch';
+import optionNone from './../utils/option-none';
 import applyWithSelect from './../utils/with-select';
+import { SLUG, PREFIX } from './../utils/prefix';
 
 /**
  * WordPress dependencies
@@ -23,12 +26,42 @@ export default compose( applyWithSelect )(
 
 		getPosts = async () => {};
 
+		// Filter and generate a list post-type select-options.
+		getPostTypes = () => {
+			const optionKeys = { slug: 'value', name: 'label' },
+				{ postTypes } = this.props,
+				typeList = chain( postTypes )
+					.filter( 'viewable' )
+					.map( ( post ) => pick( post, [ 'slug', 'name' ] ) )
+					.map( ( post ) => mapKeys( post, ( value, key ) => optionKeys[ key ] ) )
+					.value();
+			return concat( optionNone, typeList );
+		};
+
 		render() {
-			const { isSelected, className, attributes } = this.props;
+			const { isSelected, className, attributes, setAttributes, postTypes } = this.props;
+			const { type, id } = attributes;
 
 			return (
 				<Fragment>
-					<div className={ className }>AA</div>
+					<div className={ className }>
+						<Placeholder
+							icon={ <Dashicon icon="admin-post" /> }
+							className={ `${ SLUG }-placeholder` }
+							label={ __( 'Post', 'kettler-extras' ) }
+							instructions={ __(
+								'Select a post to display from the dropdown menu below:',
+								'insert-post-block'
+							) }
+						>
+							<SelectControl
+								label={ __( 'Post type:', 'insert-post-block' ) }
+								options={ this.getPostTypes() }
+								onChange={ ( value ) => setAttributes( { type: value } ) }
+								value={ type }
+							/>
+						</Placeholder>
+					</div>
 				</Fragment>
 			);
 		}

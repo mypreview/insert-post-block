ba4ffdb9d5d78f9b13b0f32d65dd947880929684
Mahdi Yazdani <mahdiyazdani@msn.com>
2020-11-11 06:17:59 +0300

    Basic component structure

--- src/components/Edit.jsx
+++ src/components/Edit.jsx
@@ -2,22 +2,35 @@
  * External dependencies
  */
 import classnames from 'classnames';
+import restFetch from './../utils/rest-fetch';
+import applyWithSelect from './../utils/with-select';
 
 /**
  * WordPress dependencies
  */
 const { __ } = wp.i18n;
 const { Fragment, Component, RawHTML } = wp.element;
+const { compose } = wp.compose;
 const { Placeholder, SelectControl, Dashicon, Notice, Spinner, Button, Disabled } = wp.components;
 const { decodeEntities } = wp.htmlEntities;
 const QUERY_ARGS = { per_page: -1 };
 
-export default class Edit extends Component {
-	render() {
-		return (
-			<Fragment>
-				<div className={ className }>CONTENT</div>
-			</Fragment>
-		);
+export default compose( applyWithSelect )(
+	class Edit extends Component {
+		componentDidMount() {
+			this.getPosts();
+		}
+
+		getPosts = async () => {};
+
+		render() {
+			const { isSelected, className, attributes } = this.props;
+
+			return (
+				<Fragment>
+					<div className={ className }>AA</div>
+				</Fragment>
+			);
+		}
 	}
-}
+);

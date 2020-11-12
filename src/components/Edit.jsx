b84df3bf32fbb38385aac93425ce1ef659b79b58
Mahdi Yazdani <mahdiyazdani@msn.com>
2020-11-12 05:03:28 +0300

    Added `Post Type` dropdown select-control

--- src/components/Edit.jsx
+++ src/components/Edit.jsx
@@ -1,7 +1,7 @@
 /**
  * External dependencies
  */
-import { concat, chain, filter, map, mapKeys, pick } from 'lodash';
+import { concat, chain, filter, map, mapKeys, pick, isArray, isEmpty } from 'lodash';
 import classnames from 'classnames';
 import restFetch from './../utils/rest-fetch';
 import optionNone from './../utils/option-none';
@@ -35,32 +35,43 @@ export default compose( applyWithSelect )(
 					.map( ( post ) => pick( post, [ 'slug', 'name' ] ) )
 					.map( ( post ) => mapKeys( post, ( value, key ) => optionKeys[ key ] ) )
 					.value();
-			return concat( optionNone, typeList );
+			return [];
 		};
 
+		hasPosts( postList ) {
+			return ( isArray( postList ) && isEmpty( postList ) ) ?? true;
+		}
+
 		render() {
-			const { isSelected, className, attributes, setAttributes, postTypes } = this.props;
+			const { isSelected, className, attributes, setAttributes } = this.props;
 			const { type, id } = attributes;
+			const postTypes = this.getPostTypes();
 
 			return (
 				<Fragment>
 					<div className={ className }>
-						<Placeholder
-							icon={ <Dashicon icon="admin-post" /> }
-							className={ `${ SLUG }-placeholder` }
-							label={ __( 'Post', 'kettler-extras' ) }
-							instructions={ __(
-								'Select a post to display from the dropdown menu below:',
-								'insert-post-block'
-							) }
-						>
-							<SelectControl
-								label={ __( 'Post type:', 'insert-post-block' ) }
-								options={ this.getPostTypes() }
-								onChange={ ( value ) => setAttributes( { type: value } ) }
-								value={ type }
-							/>
-						</Placeholder>
+						{ ! this.hasPosts( postTypes ) ? (
+							<Placeholder
+								icon={ <Dashicon icon="admin-post" /> }
+								className={ `${ SLUG }-placeholder` }
+								label={ __( 'Post', 'insert-post-block' ) }
+								instructions={ __(
+									'Select a post to display from the dropdown menu below:',
+									'insert-post-block'
+								) }
+							>
+								<SelectControl
+									label={ __( 'Post type:', 'insert-post-block' ) }
+									options={ postTypes }
+									onChange={ ( value ) => setAttributes( { type: value } ) }
+									value={ type }
+								/>
+							</Placeholder>
+						) : (
+							<Notice status="error" isDismissible={ false }>
+								{ __( 'No viewable post type found.', 'insert-post-block' ) }
+							</Notice>
+						) }
 					</div>
 				</Fragment>
 			);

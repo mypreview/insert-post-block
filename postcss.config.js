cb1d6654432b93e353074741d00c49d4f016c70b
Mahdi Yazdani <mahdiyazdani@msn.com>
2020-11-12 05:03:05 +0300

    Added plugins for the `PostCSS` loader

---
+++ postcss.config.js
@@ -0,0 +1,15 @@
+module.exports = ( { env } ) => ( {
+	plugins: {
+		'postcss-nested-ancestors': {},
+		'postcss-nested': {},
+		'postcss-if-media': {},
+		'postcss-calc': {},
+		'postcss-size': {},
+		'postcss-flexbox': {},
+		'postcss-position': {},
+		'postcss-hidden': {},
+		'postcss-selector-not': {},
+		'postcss-combine-media-query': {},
+		autoprefixer: {},
+	},
+} );

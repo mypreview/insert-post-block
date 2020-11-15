a1aed2bf40bf11e4f4caecca8c3ac7d184a516d0
Mahdi Yazdani <mahdiyazdani@msn.com>
2020-11-16 01:14:02 +0300

    Filter and generate a list post-type select-options

--- src/components/edit.js
+++ src/components/edit.js
@@ -1,10 +1,11 @@
 /**
  * External dependencies
  */
-import { concat, chain, filter, map, mapKeys, pick, isArray, isEmpty } from 'lodash';
+import { filter, isEmpty } from 'lodash';
 import classnames from 'classnames';
+import ifArray from './../utils/if-array';
 import restFetch from './../utils/rest-fetch';
-import optionNone from './../utils/option-none';
+import generateOptions from './../utils/generate-options';
 import applyWithSelect from './../utils/with-select';
 import { SLUG, PREFIX } from './../utils/prefix';
 
@@ -16,65 +17,76 @@ const { Fragment, Component, RawHTML } = wp.element;
 const { compose } = wp.compose;
 const { Placeholder, SelectControl, Dashicon, Notice, Spinner, Button, Disabled } = wp.components;
 const { decodeEntities } = wp.htmlEntities;
-const QUERY_ARGS = { per_page: -1 };
 
-export default compose( applyWithSelect )(
-	class Edit extends Component {
-		componentDidMount() {
-			this.getPosts();
-		}
+class Edit extends Component {
+	state = {
+		typeQuery: [],
+		typeList: [],
+		isLoading: true,
+	};
 
-		getPosts = async () => {};
-
-		// Filter and generate a list post-type select-options.
-		getPostTypes = () => {
-			const optionKeys = { slug: 'value', name: 'label' },
-				{ postTypes } = this.props,
-				typeList = chain( postTypes )
-					.filter( 'viewable' )
-					.map( ( post ) => pick( post, [ 'slug', 'name' ] ) )
-					.map( ( post ) => mapKeys( post, ( value, key ) => optionKeys[ key ] ) )
-					.value();
-			return [];
-		};
-
-		hasPosts( postList ) {
-			return ( isArray( postList ) && isEmpty( postList ) ) ?? true;
-		}
-
-		render() {
-			const { isSelected, className, attributes, setAttributes } = this.props;
-			const { type, id } = attributes;
-			const postTypes = this.getPostTypes();
-
-			return (
-				<Fragment>
-					<div className={ className }>
-						{ ! this.hasPosts( postTypes ) ? (
-							<Placeholder
-								icon={ <Dashicon icon="admin-post" /> }
-								className={ `${ SLUG }-placeholder` }
-								label={ __( 'Post', 'insert-post-block' ) }
-								instructions={ __(
-									'Select a post to display from the dropdown menu below:',
-									'insert-post-block'
-								) }
-							>
-								<SelectControl
-									label={ __( 'Post type:', 'insert-post-block' ) }
-									options={ postTypes }
-									onChange={ ( value ) => setAttributes( { type: value } ) }
-									value={ type }
-								/>
-							</Placeholder>
-						) : (
-							<Notice status="error" isDismissible={ false }>
-								{ __( 'No viewable post type found.', 'insert-post-block' ) }
-							</Notice>
-						) }
-					</div>
-				</Fragment>
-			);
-		}
+	componentDidMount() {
+		this.getTypes();
 	}
-);
+
+	// Filter and generate a list post-type select-options.
+	getTypes = async () => {
+		const typeQuery = await restFetch( 'types' );
+
+		this.setState( {
+			typeQuery,
+			typeList: generateOptions( typeQuery, { slug: 'value', name: 'label' } ),
+			isLoading: false,
+		} );
+	};
+
+	isLoading = ( isLoading ) => {
+		this.setState( {
+			isLoading,
+		} );
+	};
+
+	render() {
+		const { typeList, isLoading } = this.state,
+			{ isSelected, className, attributes, setAttributes } = this.props,
+			{ type, id } = attributes,
+			hasTypeList = ifArray( typeList );
+
+		return (
+			<Fragment>
+				<div className={ className }>
+					<Placeholder
+						icon={ <Dashicon icon="admin-post" /> }
+						className={ `${ SLUG }-placeholder` }
+						label={ __( 'Post', 'insert-post-block' ) }
+						instructions={ __(
+							'Select a post to display from the dropdown menu below:',
+							'insert-post-block'
+						) }
+					>
+						{ ! hasTypeList && !! isLoading ? (
+							<Spinner />
+						) : (
+							<Fragment>
+								{ ! hasTypeList && ! isLoading ? (
+									<Notice status="warning" isDismissible={ false }>
+										{ __( 'No post types found.', 'insert-post-block' ) }
+									</Notice>
+								) : (
+									<SelectControl
+										label={ __( 'Post type:', 'insert-post-block' ) }
+										options={ typeList }
+										onChange={ ( value ) => setAttributes( { type: value } ) }
+										value={ type }
+									/>
+								) }
+							</Fragment>
+						) }
+					</Placeholder>
+				</div>
+			</Fragment>
+		);
+	}
+}
+
+export default compose( [ applyWithSelect ] )( Edit );

c29f638343b25881fef50d50d91aa3622f9fcd5f
Mahdi Yazdani <mahdiyazdani@msn.com>
2020-11-18 23:25:31 +0300

    Save block shortcode

---
+++ src/save.js
@@ -0,0 +1,20 @@
+/**
+ * External dependencies
+ */
+import { SLUG } from './utils/prefix';
+
+/**
+ * WordPress dependencies
+ */
+const { RawHTML } = wp.element;
+
+const save = ( { attributes } ) => {
+	const { type, id } = attributes,
+	shortcode = `[${ SLUG } type="${ type }" id="${ id }"]`;
+
+	return (
+		<RawHTML>{ shortcode }</RawHTML>
+	);
+};
+
+export default save;

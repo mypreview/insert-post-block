d93015a21a526f8eb2dbfc9930e0ae94b718104d
Mahdi Yazdani <mahdiyazdani@msn.com>
2020-11-24 13:04:06 +0300

    Code tidy

--- src/controls.js
+++ src/controls.js
@@ -2,30 +2,25 @@
  * WordPress dependencies
  */
 const { __ } = wp.i18n;
-const { Fragment, Component } = wp.element;
 const { BlockControls } = wp.blockEditor;
 const { Toolbar, Dashicon } = wp.components;
 
-class Controls extends Component {
-	render() {
-		const { type, fetchPosts } = this.props;
-
-		return (
-			<Fragment>
-				<BlockControls>
-					<Toolbar
-						controls={ [
-							{
-								icon: <Dashicon icon="edit" />,
-								title: __( 'Edit', 'insert-post-block' ),
-								onClick: () => fetchPosts( type ),
-							},
-						] }
-					/>
-				</BlockControls>
-			</Fragment>
-		);
-	}
-}
+const Controls = ( type, fetchPosts ) => {
+	return (
+		<>
+			<BlockControls>
+				<Toolbar
+					controls={ [
+						{
+							icon: <Dashicon icon="edit" />,
+							title: __( 'Edit', 'insert-post-block' ),
+							onClick: () => fetchPosts( type ),
+						},
+					] }
+				/>
+			</BlockControls>
+		</>
+	);
+};
 
 export default Controls;
